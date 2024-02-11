3d51015ea210c317d1489733fd46d948f08a8d71
Vitor Natario <natariovitor@gmail.com>
2024-02-10 21:05:35 -0300

    feat: cretae modal to new-note-card

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,10 +1,54 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
+
 export function NewNoteCard() {
   return (
-    <div className="rounded-md bg-slate-700 p-5 space-y-3 ">
-      <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Grave uma nota em áudio que será convertida para texto automaticamente.
-      </p>
-    </div>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md bg-slate-700 p-5 text-left flex flex-col gap-3 ">
+        <span className="text-sm font-medium text-slate-200">
+          Adicionar nota
+        </span>
+        <p className="text-sm leading-6 text-slate-400">
+          Grave uma nota em áudio que será convertida para texto
+          automaticamente.
+        </p>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-black/60" />
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-2xl w-full h-[60vh] rounded-md bg-slate-700 flex flex-col outline-none">
+          <Dialog.Close className="absolute top-0 right-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-200">
+              Adicionar nota
+            </span>
+
+            <p className="text-sm leading-6 text-slate-400">
+              Comece{' '}
+              <button className="text-lime-400 font-medium hover:underline">
+                gravando uma nota
+              </button>{' '}
+              em áudio ou se preferir{' '}
+              <button className="text-lime-400 font-medium hover:underline">
+                utilize apenas texto
+              </button>
+              .
+            </p>
+          </div>
+
+          <button
+            type="button"
+            className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+          >
+            Salvar nota
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
