4def6111641b8640ebe482dc4304f794baa6344e
Vitor Natario <natariovitor@gmail.com>
2024-02-12 23:05:50 -0300

    style: add mobile support

--- src/app.tsx
+++ src/app.tsx
@@ -49,7 +49,7 @@ export function App() {
       : notes
 
   return (
-    <div className="max-w-6xl mx-auto my-12 space-y-6">
+    <div className="max-w-6xl mx-auto my-12 space-y-6 px-5 ">
       <img src={logo} alt="NLW Expert" />
 
       <form className="">
@@ -65,7 +65,7 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {filteredNotes.map((note) => {
           return <NoteCard key={note.id} note={note} />
