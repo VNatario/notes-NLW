40261644cc45e27de35d69c4ebc781b2b5886905
Vitor Natario <natariovitor@gmail.com>
2024-02-12 23:21:19 -0300

    feat: add delete note function

--- src/app.tsx
+++ src/app.tsx
@@ -35,6 +35,11 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function onNoteDelete(id: string) {
+    const notesArray = notes.filter((note) => note.id !== id)
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
   function handleSearch(event: ChangeEvent<HTMLInputElement>) {
     setSearchNotes(event.target.value)
   }
@@ -68,7 +73,9 @@ export function App() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {filteredNotes.map((note) => {
-          return <NoteCard key={note.id} note={note} />
+          return (
+            <NoteCard key={note.id} note={note} onNoteDelete={onNoteDelete} />
+          )
         })}
       </div>
     </div>
