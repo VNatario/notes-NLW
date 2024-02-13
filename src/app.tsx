f653f4072ff35863990d706d13f64162c4210c54
Vitor Natario <natariovitor@gmail.com>
2024-02-12 22:01:09 -0300

    feat: save notes

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,27 @@
+import { useState } from 'react'
 import logo from './assets/logo.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
-const note = {
-  date: new Date(),
-  content: 'Teste',
+interface Note {
+  id: string
+  date: Date
+  content: string
 }
+
 export function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    setNotes((prevState) => [newNote, ...prevState])
+  }
+
   return (
     <div className="max-w-6xl mx-auto my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
@@ -24,8 +39,10 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-        <NewNoteCard />
-        <NoteCard note={note} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {notes.map((note) => {
+          return <NoteCard key={note.id} note={note} />
+        })}
       </div>
     </div>
   )
